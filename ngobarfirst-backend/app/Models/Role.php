53f8439f15707403fc833a786585e3736d3f429e
ganasa18 <ganasa18@yahoo.com>
2022-04-23 10:54:20 +0700

    setup database perpus remake (2)

---
+++ ngobarfirst-backend/app/Models/Role.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
+
+class Role extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'role',
+    ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+}
