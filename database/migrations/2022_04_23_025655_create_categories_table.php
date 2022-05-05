04f4e45e4df0ad7bcf1e4d7530b5d159bc85ed6d
ganasa18 <ganasa18@yahoo.com>
2022-05-05 17:12:45 +0700

    updated role 3

--- database/migrations/2022_04_23_025655_create_categories_table.php
+++ database/migrations/2022_04_23_025655_create_categories_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('category')->nullable();
+
+            $table->softDeletes();
             $table->timestamps();
         });
     }
